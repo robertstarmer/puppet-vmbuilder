0604c17b6f82aea3df5b7586996d12ff7601ece7
Robert Starmer <starmer@cisco.com>
2013-04-06 02:32:23 -0700

    reorder tre-boot commands

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,8 +39,8 @@ class vmbuild (
   } ->
   exec { "vmbuild-kvm-$hostname":
    path => ["/usr/bin","/bin","/sbin"],
-   #command => "vmbuilder kvm ubuntu --mask 255.255.255.0 --net 192.168.40.0 --bcast 192.168.40.255 --gw 192.168.40.1 --dns 192.168.40.254 --firstboot=/etc/vmbuilder.boot.sh --hostname=build-vm --destdir=/vms/build-vm --ip 192.168.40.5",
-   command => "vmbuilder kvm ubuntu --firstboot=/etc/vmbuilder.boot.sh --mask $netmask --net $net --gw $gateway --dns $dns --hostname=$hostname --destdir=/vms/$hostname --ip $ip",
+   timeout => '1200',
+   command => "vmbuilder kvm ubuntu --firstboot=/etc/vmbuilder.boot.sh --mask $netmask --net $net --gw $gateway --dns $dns --hostname=$hostname --destdir=/vms/$hostname --ip $ip --firstboot=/etc/vmbuilder.boot.sh",
    unless => "test -d /vms/$hostname",
   }
 }
