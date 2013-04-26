0b7a3c75f5e86fc8d74d67a3d9d2fbd865191ff7
Robert Starmer <starmer@cisco.com>
2013-04-25 23:27:27 -0700

    Update init to include vnc xstartup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class vmbuild (
+class vmbuilder (
  $distribution = 'precise',
  $mirror = undef,
  $network = '192.168.1.0',
@@ -10,7 +10,7 @@ class vmbuild (
  $domain = 'example.com',
  $disk = '8192',
  $ram = '4096',
- $bridge = 'br0',
+ $bridge = 'br-ex',
  $puppetmaster ='build-server.example.com',
 ) {
 
@@ -40,7 +40,24 @@ class vmbuild (
   exec { "vmbuild-kvm-$hostname":
    path => ["/usr/bin","/bin","/sbin"],
    timeout => '1200',
-   command => "vmbuilder kvm ubuntu --firstboot=/etc/vmbuilder.boot.sh --mask $netmask --net $network --gw $gateway --dns $dns --hostname=$hostname --destdir=/vms/$hostname --ip $ip",
+   #command => "vmbuilder kvm ubuntu --firstboot=/etc/vmbuilder.boot.sh --mask $netmask --net $network --gw $gateway --dns $dns --hostname=$hostname --destdir=/vms/$hostname --ip $ip",
+   command => "vmbuilder kvm ubuntu  --mask $netmask --net $network --gw $gateway --dns $dns --hostname=$hostname --destdir=/vms/$hostname --ip $ip",
    unless => "test -d /vms/$hostname",
   }
+  file { "/root/.vnc/':
+    ensure => 'directory',
+  } ->
+  file { "/root/.vnc/xstartup':
+    ensure => present,
+    content => "#!/bin/sh
+
+unset SESSION_MANAGER
+gnome-session –session=gnome-classic &
+
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+vncconfig -iconic &
+"
+    }
+
 }
